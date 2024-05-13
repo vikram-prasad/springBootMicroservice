88ce3b4cafa3e7cad8192180634f58f576b2086f
Vikram Prasad <vik1narayan@gmail.com>
2024-05-13 19:27:21 +0530

    naming server

---
+++ naming-server/naming-server/src/test/java/com/vikram/microservice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.microservice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
